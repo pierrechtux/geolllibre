7e56a3d4c2e6bbab240185d848d9a4913d178b7d
Pierre <pierrechevaliergeol@free.fr>
2013-11-06 12:02:06 +0100

    gll_geolpda_fetch_data.r works: it (sorts of) synchronizes between geolpda device and bdexplo tables public.field_observations and public.field_observations_struct_measures.

--- gll_geolpda_fetch_data.r
+++ gll_geolpda_fetch_data.r
@@ -67,7 +67,6 @@ synchronize_geolpda: does [
 do %~/rebol/library/scripts/btn-sqlite.r
 ;}}}
 
-
 ; First, connect geolpda android device, copy to local directory:{{{ } } }
 ; Get the user to properly mount the android device:
 alert "Mount android device: connect android device containing geolpda; then press Enter when device properly connected"
@@ -80,15 +79,13 @@ either DEBUG [	dir_geolpda_android: %/mnt/galaxy1/geolpda/ ] [
 ; Get the location of the local image of geolpda data (unless DEBUG is on):
 alert {now locate the local directory where geolpda data is (or will be) replicated}
 
-either DEBUG [ 	dir_geolpda_local:  %~/geolpda/android_cp/geolpda/	] [
+either DEBUG [	dir_geolpda_local:  %~/geolpda/android_cp/geolpda/	] [
 				dir_geolpda_local: request-dir/title {locate local directory for replication of geolpda data} ]
 
 
 print rejoin ["Mount directory of GeolPDA android device: " tab tab dir_geolpda_android newline "Local directory for GeolPDA data replication: " tab dir_geolpda_local]
 
 ;}}}
-;___________________JEANSUILA___________________
-
 ; Synchronize android device to local filesystem, if agreed (and not DEBUG):{{{ } } }
 unless DEBUG [ if ( confirm "synchronize geolpda data?" ) [ synchronize_geolpda ] ]
 ;}}}
@@ -96,13 +93,17 @@ unless DEBUG [ if ( confirm "synchronize geolpda data?" ) [ synchronize_geolpda 
 print "Open GeolPDA database..."
 change-dir dir_geolpda_local
 copy-file %geolpda %geolpda_copy.db
-	; => not terrible; just for the fact that driver does
+	; => not terrible; this file copy is /only due to the 
+	;    fact that the btn (better than nothing) driver does
 	;    not support sqlite file without extension...
 db: open to-url rejoin [{btn://localhost/} dir_geolpda_local {geolpda_copy.db}]
 
 ; Get data: as db is the same name as defined for default
 ; database connexion in gll_routines.r, we can use the functions:
+; observations: {{{ } } }
 run_query "SELECT * FROM poi"
+	; DEBUG TODO remove ça
+	; write %qq1 sql_result_csv
 
 ; Comparison of field list: to be sure that the table structure matches the 
 ; one used at the time of coding (23-Oct-2013/9:24:01+2:00)
@@ -111,14 +112,18 @@ unless sql_result_fields = ["_id" "poiname" "poitime" "elevation" "poilat" "poil
 	print "Error, halting"
 	halt
 ]
-geolpda_observations: 		copy sql_result
-geolpda_observations_fields: 	copy sql_result_fields
+geolpda_observations:        copy sql_result
+geolpda_observations_fields: copy sql_result_fields
 print rejoin [tab length? geolpda_observations " records in observations table"]
 
 ; To check if a record was previously inserted in the database, make a copy of identifiers:
 run_query "SELECT poiname, _id, poitime FROM poi"
-tt_observations_in_geolpda: copy sql_result
+	; DEBUG TODO remove ça
+	; write %qq2 sql_result_csv
 
+tt_observations_in_geolpda: copy sql_result
+;}}}
+; orientations:{{{ } } }
 run_query "SELECT * FROM orientation"
 ; Comparison of field list: to be sure that the table structure matches the 
 ; one used at the time of coding (23-Oct-2013/9:24:01+2:00)
@@ -127,16 +132,23 @@ unless sql_result_fields = ["_id" "poi_id" "orientationtype" "rot1" "rot2" "rot3
 	print "Error, halting"
 	halt
 ]
+; If we reached here, we are ok; now, it is necessary to also fetch the full id from observations by JOINing:
+run_query "SELECT poiname, orientation._id, poi_id, orientationtype, rot1, rot2, rot3, rot4, rot5, rot6, rot7, rot8, rot9, v1, v2, v3 FROM orientation LEFT JOIN poi ON poi._id = orientation.poi_id"
 geolpda_orientations: 			copy sql_result
 geolpda_orientations_fields: 	copy sql_result_fields
 print rejoin [tab length? geolpda_orientations " records in orientations measurements table"]
+	; DEBUG TODO remove ça
+	; write %qq3 sql_result_csv
 
 ; To check if a record was previously inserted in the database, make a copy of identifiers:
 ;run_query "SELECT poi_id FROM orientation"
-; => 	ah, no: in final database, the whole id is mentioned, not the index;
-; 		so we must JOIN the two tables:
+; =>    ah, no: in final database, the whole id is mentioned, not the index;
+;       so we must JOIN the two tables:
 run_query "SELECT poiname, count(*) FROM orientation LEFT JOIN poi ON poi._id = orientation.poi_id GROUP BY poiname"
 tt_orientations_in_geolpda: copy sql_result
+
+	; DEBUG TODO remove ça
+	; write %qq4 sql_result_csv
 
 ; The sqlite driver apparently returns strings instead of numerics:
 ; conversion to decimal! (same as value returned by postgresql driver), 
@@ -147,6 +159,7 @@ foreach v tt_orientations_in_geolpda [
 ]
 tt_orientations_in_geolpda: copy tt
 ;}}}
+;}}}
 
 ; Open destination database:{{{ } } }
 connection_db
@@ -155,14 +168,19 @@ print rejoin [newline "Open database " dbname " on host " dbhost]
 ; => careful: now DB points to the default database, not to the geolpda any more.
 insert db "BEGIN TRANSACTION;"
 run_query "SELECT obs_id, waypoint_name, timestamp_epoch_ms FROM field_observations"
+	; DEBUG TODO remove ça
+	; write %qq5 sql_result_csv
+
 tt_observations_already_in_db: copy sql_result
 print rejoin [tab length? tt_observations_already_in_db " records in field_observations table"]
 
 run_query "SELECT obs_id, count(*) FROM field_observations_struct_measures GROUP BY obs_id"
 tt_orientations_already_in_db: copy sql_result
+	; DEBUG TODO remove ça
+	; write %qq6 sql_result_csv
+
 
 ;}}}
-
 ; Remove records from dataset from geolpda which are already in database: {{{ } } }
 ; observations: {{{ } } }
 print rejoin [newline "Remove records from dataset from geolpda which are already in database:" newline
@@ -172,17 +190,15 @@ tab length? tt_observations_already_in_db " records in field_observations table:
 
 ; Strange: a trailing ".0" appeared at the end of the epoch timestamp, from the
 ; postgresql database side, even though the field type is a bigint:
-; 	=>	figured out: rebol saw this bigint as a decimal! ; but in
-; 		the geolpda database, the epoch timestamp is returned as 
-;		a string!
+;   =>  figured out: rebol saw this bigint as a decimal! ; but in
+;       the geolpda database, the epoch timestamp is returned as 
+;       a string!
 ; workaround: suppress these ".0":
 tt: copy []
 foreach i tt_observations_already_in_db [
 	either (none? i/3) [
-		x: none
-	][
-		x: replace to-string i/3 ".0" ""
-	]
+		x: none								][
+		x: replace to-string i/3 ".0" ""	]
 	;print rejoin [x i/3 type? i/3 none? i/3]
 	append/only tt to-block reduce [i/1 i/2 x]
 ]
@@ -213,8 +229,8 @@ tt_orientations_already_in_db: 	unique tt_orientations_already_in_db	; (normally
 to_del: intersect tt_orientations_in_geolpda tt_orientations_already_in_db
 ;write %~/qa newline
 ;write %~/qb newline
-;foreach i tt_orientations_in_geolpda 	[	write/append %~/qa rejoin [i/1 " " i/2 newline]	]
-;foreach i tt_orientations_already_in_db [	write/append %~/qb rejoin [i/1 " " i/2 newline]	]
+;foreach i tt_orientations_in_geolpda 	[	write/append %~/qa rejoin [i/1 " " i/2 newline] ]
+;foreach i tt_orientations_already_in_db [	write/append %~/qb rejoin [i/1 " " i/2 newline] ]
 ;vimdiff qa qb
 
 print rejoin [	tab tab length? to_del " common records (sharing same values for (obs_id, waypoint_name, timestamp_epoch_ms) fields): " 
@@ -234,88 +250,92 @@ unless ((length? to_del) = 0) [
 ]
 print rejoin [tab tab length? geolpda_orientations " records to be INSERTed into database " dbname " on " dbhost "."]
 ;}}}
-
-;___________________JEANSUILA___________________
-;####################################################################{{{
-; ###### data from geolpda is now in tables:
-; 	geolpda_observations
-;	geolpda_orientations
-; check recordsets:
-; 	tt_observations_in_geolpda       <------------------+ok
-;	tt_orientations_in_geolpda            <-------+ok   |
-; ###### check recordsets from database:          |     |
-;	tt_observations_already_in_db     <-----------^-----+
-;	tt_orientations_already_in_db           <-----+
-
-;	/tt_observations_in_geolpda\|tt_observations_already_in_db
-;	/tt_orientations_in_geolpda\|tt_orientations_already_in_db
-
-;####################################################################
-;/*}}}*/
-{{{ } } }
-print rejoin [	newline "Remove records from dataset from geolpda which are already in database:" newline
-				tab length? tt_observations_in_geolpda " records in GeolPDA dataset,"
-				tab length? length? tt_observations_already_in_db " records in field_observations table:"]
-
-; Strange: a trailing ".0" appeared at the end of the epoch timestamp, from the
-; postgresql database side, even though the field type is a bigint:
-; 	=>	figured out: rebol saw this bigint as a decimal! ; but in
-; 		the geolpda database, the epoch timestamp is returned as 
-;		a string!
-; workaround: suppress these ".0":
-tt: copy []
-foreach i tt_observations_already_in_db [
-	either (none? i/3) [
-		x: none
-	][
-		x: replace to-string i/3 ".0" ""
-	]
-	print rejoin [x i/3 type? i/3 none? i/3]
-	append/only tt to-block reduce [i/1 i/2 x]
-]
-tt_observations_already_in_db: copy tt
-to_del: intersect tt_observations_in_geolpda tt_observations_already_in_db
-print rejoin [	tab tab length? to_del " common records (sharing same values for (obs_id, waypoint_name, timestamp_epoch_ms) fields): " 
-				newline tab tab "=> remove from dataset..."]
-observations_new: exclude tt_observations_in_geolpda tt_observations_already_in_db
-
-tt: copy []
-foreach o geolpda_observations [
-	foreach n observations_new [
-		if (all [(n/1 = o/2) (n/2 = o/1) (n/3 = o/3)]) [
-			; record to be kept
-			append/only tt o
-			break
-]	]	]
-geolpda_observations: copy tt
-print rejoin [tab tab length? geolpda_observations " records to be INSERTed into database " dbname " on " dbhost "."]
+;}}}
+; Generate a new datasource: TODO: {{{ } } }
+datasource: 9999    ; bidon; TODO generate a new datasource record in lex_datasource
 ;}}}
 
+;####################################################################{ {{
+; ###### data from geolpda is now in tables:
+;   geolpda_observations
+;   geolpda_orientations
+; check recordsets:
+;   tt_observations_in_geolpda       <------------------+ok
+;   tt_orientations_in_geolpda            <-------+ok   |
+; ###### check recordsets from database:          |     |
+;   tt_observations_already_in_db     <-----------)-----+
+;   tt_orientations_already_in_db           <-----+
 
+;   /tt_observations_in_geolpda\|tt_observations_already_in_db
+;   /tt_orientations_in_geolpda\|tt_orientations_already_in_db
 
-; put data:
-; observations:
+;####################################################################
+;/*} }}*/
+
+; Put data:{{{ } } }
+; observations:{{{ } } }
 ; build a SQL INSERT statement:
-sep: {, } ; just to save a few keystrokes to the coder... and to make REJOIN code a bit more readable
-sql_string:  {INSERT INTO public.field_observations (opid,year,obs_id,date,waypoint_name,x,y,z,description,code_litho,code_unit,srid,geologist,icon_descr,comments,sample_id,datasource,numauto,photos,audio,timestamp_epoch_ms) VALUES }
+_: {, } ; just to save a few keystrokes to the coder... and to make REJOIN code a bit more readable; variable was called SEP for SEParator; renamed to _ for clarity/brevity
+sql_string:  {INSERT INTO public.field_observations (opid,year,obs_id,date,waypoint_name,x,y,z,description,code_litho,code_unit,srid,geologist,icon_descr,comments,sample_id,datasource,photos,audio,timestamp_epoch_ms) VALUES }
 foreach o geolpda_observations [
-	; assign temporary variables with field names: (bof, no)
+	; assign temporary variables with field names: (bof, no){{{ } } }
 	;i: 0
 	;foreach f geolpda_observations_fields [
 	;	++ i
 	;	print rejoin [f ": "]
 	;	print o/:i
-	;]
-;___________________
+	;] ; }}}
 	; check if record already in database or not: by default, we do not update
 	; data in the database, assuming that data inside the database could have
 	; changed, corrected, updated, etc. since it was imported from GeolPDA.
-
+	; DEBUG o: geolpda_observations/4
 	tmp: to-date epoch-to-date (to-integer ((to-decimal o/3) / 1000))
-	append sql_string
-	rejoin [newline {(} opid SEP tmp/year SEP {'} o/2 {'} SEP {'} tmp/year "-" pad tmp/month 2 "-" pad tmp/day 2 {'} SEP {'} o/1 {'} SEP o/6 SEP o/5 SEP o/4 SEP {'} o/9 {'} SEP {NULL, NULL, 4326} SEP {'} geologist {'} SEP {NULL, NULL, NULL, NULL, } {'} o/7 {'} SEP {'} o/8 {'} SEP o/3 {),}]
+	append sql_string rejoin [newline {(} opid _ tmp/year _ {'} o/2 {'} _ {'} tmp/year "-" pad tmp/month 2 "-" pad tmp/day 2 {'} _ {'} o/1 {'} _ o/6 _ o/5 _ o/4 _ {'} o/9 {'} _ {NULL, NULL, 4326} _ {'} geologist {'} _ {NULL, NULL, NULL, NULL, } {'} o/7 {'} _ {'} o/8 {'} _ o/3 {),}]
 ]
-sql_string: rejoin [copy/part sql_string ((length? sql_string) - 2) ";"]
+sql_string: rejoin [copy/part sql_string ((length? sql_string) - 1) ";"]
+
+;DEBUG editor sql_string
+
+print "SQL statement to be run:"
+print copy/part sql_string 300
+print "..."
+
+
+; DEBUG vérif:
+; run_query "SELECT * FROM public.field_observations"
+
+;write %azeq  newline
+;ww: copy sql_result
+;foreach r ww [
+;	write/append %azeq rejoin [r newline]
+;]
+;}}}
+; orientations: {{{ } } }
+; continue the SQL INSERT statement:
+	;\pset format unaligned
+	;\pset fieldsep ','
+	;SELECT * FROM public.field_observations_struct_measures LIMIT 0;
+	;opid,obs_id,measure_type,structure_type,north_ref,direction,dip,dip_quadrant,pitch,pitch_quadrant,movement,valid,comments,numauto,db_update_timestamp,username,datasource,rotation_matrix,geolpda_id,geolpda_poi_id
+
+; DEBUG 
+; o: geolpda_orientations/1
+; geolpda_orientations_fields
+; == ["poiname" "_id" "poi_id" "orientationtype" "rot1" "rot2" "rot3" "rot4" "rot5" "rot6" "rot7" "rot8" "rot9" "v1" "v2" "v3"]
+;   m/       1     2        3                 4      5      6      7      8      9     10     11     12     13   14   15   16
+;for i 5 13 1 [prin rejoin [" m/" to-string i { " "}]]
+; m/5 " " m/6 " " m/7 " " m/8 " " m/9 " " m/10 " " m/11 " " m/12 " " m/13 " ">> 
+append sql_string rejoin [newline newline {INSERT INTO public.field_observations_struct_measures (opid,    obs_id     , measure_type , north_ref ,  datasource ,        rotation_matrix                                                                ,   geolpda_id , geolpda_poi_id   ) VALUES } ]
+foreach m geolpda_orientations [
+	append sql_string rejoin [newline {(}                                               		  opid _ {'} m/1 {'}  _  {'} m/4 {'} _    {'Nm'} _  datasource _   {'[}  m/5 " " m/6 " " m/7 " " m/8 " " m/9 " " m/10 " " m/11 " " m/12 " " m/13  {]'} _       m/2    _      m/3        {),}        ]
+]
+sql_string: rejoin [copy/part sql_string ((length? sql_string) - 1) ";"]
+		; DEBUG 
+		;editor sql_string
+		;sql_string: copy ""
+
+		;write %azeq sql_string
+		; DEBUG
+
 
 print "SQL statement to be run:"
 print copy/part sql_string 300
@@ -324,12 +344,34 @@ print "..."
 insert db sql_string
 print "Done..."
 insert db "COMMIT;"
+
+; DEBUG vérif:
+run_query "SELECT * FROM public.field_observations"
+write %azeq  newline
+ww: copy sql_result
+foreach r ww [
+	write/append %azeq rejoin [mold r newline]
+]
+
+run_query "SELECT * FROM public.field_observations_struct_measures"
+write/lines %aqez sql_result_csv
+sql_result
+ %azeq  newline
+ww: copy sql_result
+foreach r ww [
+	write/append %azeq rejoin [mold r newline]
+]
+
+
+;}}}
+;}}}
+
+; Play INSERT queries on database and COMMIT: {{{ } } }
+insert db sql_string
+print "Done..."
+insert db "COMMIT;"
+
 print "commited => end."
 
-
-
-; orientations:
-
-
-
-
+;}}}
+; Finished.
