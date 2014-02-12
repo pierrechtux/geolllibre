de829a1485aaa7be4dac3b4f4903b5fc6bbc8c51
Pierre <pierrechevaliergeol@free.fr>
2014-02-12 12:10:51 +0100

    Attempt to upload data based on _id, instead of comparing datasets, since it generated duplicates, somehow.

--- gll_geolpda_fetch_data.r
+++ gll_geolpda_fetch_data.r
@@ -38,6 +38,24 @@ This file is part of GeolLLibre software suite: FLOSS dedicated to Earth Science
 ];}}}
 
 DEBUG: false
+; DEBUG vérif:  ============================ sauve 2 tables obs et mesures en csv AVANT: {{{ } } }
+run_query "SELECT * FROM public.field_observations"
+;write %~/field_observations_avant.csv sql_result_csv
+write %~/field_observations_avant.csv newline
+ww: copy sql_result
+foreach r ww [
+	write/append %~/field_observations_avant.csv rejoin [mold r newline]
+]
+
+run_query "SELECT * FROM public.field_observations_struct_measures"
+;write/lines %~/field_observations_struct_measures_avant.csv sql_result_csv
+write %~/field_observations_struct_measures_avant.csv newline
+ww: copy sql_result
+foreach r ww [
+	write/append %~/field_observations_struct_measures_avant.csv rejoin [mold r newline]
+]
+;}}}
+
 ; initialisation: ;{{{ } } }
 if error? try [						; Récupération des routines (et des préférences) et connexion à la base
 if error? try [						; Récupération des routines (et des préférences) et connexion à la base
@@ -82,118 +100,18 @@ synchronize_oruxmaps_tracklogs: does [
 do %~/rebol/library/scripts/btn-sqlite.r
 ;}}}
 
-; First, connect geolpda android device, copy to local directory:{{{ } } }
-; default directories are stored in .gll_preferences
-
-; Get the user to properly mount the android device:
-alert "Mount android device: connect android device containing geolpda; then press Enter when device properly connected"
-; Get the location where it is mounted (unless DEBUG is on):
-alert {locate where android device is mounted, pick up "geolpda" subdirectory}
-unless DEBUG [ dir_mount_geolpda_android: request-dir/title/dir {locate geolpda where android device is located, choose "geolpda" subdirectory} dir_mount_geolpda_android ]
-
-; Get the location of the local image of geolpda data (unless DEBUG is on):
-alert {now locate the local directory where geolpda data is (or will be) replicated}
-unless DEBUG [ dir_geolpda_local:         request-dir/title/dir {locate local directory for replication of geolpda data}                        dir_geolpda_local         ]
-
-print rejoin ["Mount directory of GeolPDA android device: " tab tab dir_mount_geolpda_android newline "Local directory for GeolPDA data replication: " tab dir_geolpda_local]
-
-;}}}
-; Synchronize android device to local filesystem, if agreed (and not DEBUG):{{{ } } }
-unless DEBUG [ if ( confirm "synchronize geolpda data?" ) [ synchronize_geolpda ] ]
-;}}}
-; Open sqlite geolpda, get data:{{{ } } }
-print "Open GeolPDA database..."
-change-dir dir_geolpda_local
-copy-file %geolpda %geolpda_copy.db
-	; => not terrible; this file copy is /only due to the 
-	;    fact that the btn (better than nothing) driver does
-	;    not support sqlite file without extension...
-db: open to-url rejoin [{btn://localhost/} dir_geolpda_local {geolpda_copy.db}]
-
-; Get data: as db is the same name as defined for default
-; database connexion in gll_routines.r, we can use the functions:
-; observations: {{{ } } }
-run_query "SELECT * FROM poi"
-	; DEBUG TODO remove ça
-	; write %qq1 sql_result_csv
-
-; Comparison of field list: to be sure that the table structure matches the 
-; one used at the time of coding (23-Oct-2013/9:24:01+2:00)
-unless sql_result_fields = ["_id" "poiname" "poitime" "elevation" "poilat" "poilon" "photourl" "audiourl" "note"] [
-	print "ATTENTION! field names differ from geolpda reference implementation"
-	print "Error, halting"
-	halt
-]
-geolpda_observations:        copy sql_result
-geolpda_observations_fields: copy sql_result_fields
-print rejoin [tab length? geolpda_observations " records in observations table"]
-
-; To check if a record was previously inserted in the database, make a copy of identifiers:
-run_query "SELECT poiname, _id, poitime FROM poi"
-	; DEBUG TODO remove ça
-	; write %qq2 sql_result_csv
-
-tt_observations_in_geolpda: copy sql_result
-;}}}
-; orientations:{{{ } } }
-run_query "SELECT * FROM orientation"
-; Comparison of field list: to be sure that the table structure matches the 
-; one used at the time of coding (23-Oct-2013/9:24:01+2:00)
-unless sql_result_fields = ["_id" "poi_id" "orientationtype" "rot1" "rot2" "rot3" "rot4" "rot5" "rot6" "rot7" "rot8" "rot9" "v1" "v2" "v3"] [
-	print "ATTENTION! field names differ from geolpda reference implementation"
-	print "Error, halting"
-	halt
-]
-; If we reached here, we are ok; now, it is necessary to also fetch the full id from observations by JOINing:
-run_query "SELECT poiname, orientation._id, poi_id, orientationtype, rot1, rot2, rot3, rot4, rot5, rot6, rot7, rot8, rot9, v1, v2, v3 FROM orientation LEFT JOIN poi ON poi._id = orientation.poi_id"
-geolpda_orientations: 			copy sql_result
-geolpda_orientations_fields: 	copy sql_result_fields
-print rejoin [tab length? geolpda_orientations " records in orientations measurements table"]
-	; DEBUG TODO remove ça
-	; write %qq3 sql_result_csv
-
-; To check if a record was previously inserted in the database, make a copy of identifiers:
-;run_query "SELECT poi_id FROM orientation"
-; =>    ah, no: in final database, the whole id is mentioned, not the index;
-;       so we must JOIN the two tables:
-run_query "SELECT poiname, count(*) FROM orientation LEFT JOIN poi ON poi._id = orientation.poi_id GROUP BY poiname"
-tt_orientations_in_geolpda: copy sql_result
-
-	; DEBUG TODO remove ça
-	; write %qq4 sql_result_csv
-
-; The sqlite driver apparently returns strings instead of numerics:
-; conversion to decimal! (same as value returned by postgresql driver), 
-; to allow comparisons, later on:
-tt: copy []
-foreach v tt_orientations_in_geolpda [
-	append/only tt compose [(v/1) (to-decimal v/2)]
-]
-tt_orientations_in_geolpda: copy tt
-;}}}
-;}}}
-
 ; Open destination database:{{{ } } }
-connection_db
-print rejoin [newline "Open database " dbname " on host " dbhost]
-
-; => careful: now DB points to the default database, not to the geolpda any more.
-insert db "BEGIN TRANSACTION;"
-run_query "SELECT obs_id, waypoint_name, timestamp_epoch_ms FROM field_observations"
-	; DEBUG TODO remove ça
-	; write %qq5 sql_result_csv
-
+run_query "SELECT obs_id, waypoint_name, timestamp_epoch_ms FROM public.field_observations"
 tt_observations_already_in_db: copy sql_result
 print rejoin [tab length? tt_observations_already_in_db " records in field_observations table"]
 
-run_query "SELECT obs_id, count(*) FROM field_observations_struct_measures GROUP BY obs_id"
+run_query "SELECT obs_id, count(*) FROM public.field_observations_struct_measures GROUP BY obs_id"
 tt_orientations_already_in_db: copy sql_result
-	; DEBUG TODO remove ça
-	; write %qq6 sql_result_csv
-
-
+print rejoin [tab length? tt_orientations_already_in_db " observations with records in field_observations_struct_measures table"]
 ;}}}
 ; Remove records from dataset from geolpda which are already in database: {{{ } } }
+;TODO BUG: obviously does not work as expected.
+comment: [; OLD SOLUTION: IGNORED
 ; observations: {{{ } } }
 print rejoin [newline "Remove records from dataset from geolpda which are already in database:" newline
 tab "- observations:"
@@ -205,7 +123,7 @@ tab length? tt_observations_already_in_db " records in field_observations table:
 ;   =>  figured out: rebol saw this bigint as a decimal! ; but in
 ;       the geolpda database, the epoch timestamp is returned as 
 ;       a string!
-; workaround: suppress these ".0":
+; workaround: suppress these ".0": {{{ } } }
 tt: copy []
 foreach i tt_observations_already_in_db [
 	either (none? i/3) [
@@ -215,6 +133,8 @@ foreach i tt_observations_already_in_db [
 	append/only tt to-block reduce [i/1 i/2 x]
 ]
 tt_observations_already_in_db: copy tt
+;}}}
+
 to_del: intersect tt_observations_in_geolpda tt_observations_already_in_db
 print rejoin [tab tab length? to_del " common records (sharing same values for (obs_id, waypoint_name, timestamp_epoch_ms) fields): " newline tab tab "=> remove from geolpda dataset: ..."]
 unless ((length? to_del) = 0) [
@@ -229,8 +149,9 @@ unless ((length? to_del) = 0) [
 	geolpda_observations: copy tt
 ]
 print rejoin [tab tab length? geolpda_observations " records remaining to be INSERTed into database " dbname " on " dbhost "."]
-;}}}
 
+
+;}}}
 ; orientations: {{{ } } }
 print rejoin [	tab "- orientations:" newline 
 				tab length? tt_orientations_in_geolpda " records in GeolPDA dataset,"
@@ -262,7 +183,102 @@ unless ((length? to_del) = 0) [
 ]
 print rejoin [tab tab length? geolpda_orientations " records to be INSERTed into database " dbname " on " dbhost "."]
 ;}}}
+]
+; 2014_02_12__10_32_25: much more simple: get the maximum of _id in the bdexplo database, and only insert data with higher _id:
+;get the maximum of _id in the bdexplo database (the field is waypoint_name):
+run_query rejoin [{SELECT max(waypoint_name::numeric) FROM public.field_observations WHERE device = '} geolpda_device {';}]
+max_waypoint_name: to-integer to-string first (copy sql_result)
+; and, later, only consider data with higher _id
 ;}}}
+; Connect geolpda android device, copy to local directory:{{{ } } }
+; default directories are stored in .gll_preferences
+
+; Get the user to properly mount the android device:
+alert "Mount android device: connect android device containing geolpda; then press Enter when device properly connected"
+; Get the location where it is mounted (unless DEBUG is on):
+alert {locate where android device is mounted, pick up "geolpda" subdirectory}
+unless DEBUG [ dir_mount_geolpda_android: request-dir/title/dir {locate geolpda where android device is located, choose "geolpda" subdirectory} dir_mount_geolpda_android ]
+
+; Get the location of the local image of geolpda data (unless DEBUG is on):
+alert {now locate the local directory where geolpda data is (or will be) replicated}
+unless DEBUG [ dir_geolpda_local:         request-dir/title/dir {locate local directory for replication of geolpda data}                        dir_geolpda_local         ]
+
+print rejoin ["Mount directory of GeolPDA android device: " tab tab dir_mount_geolpda_android newline "Local directory for GeolPDA data replication: " tab dir_geolpda_local]
+;}}}
+; Synchronize android device to local filesystem, if agreed (and not DEBUG):{{{ } } }
+unless DEBUG [ if ( confirm "synchronize geolpda data?" ) [ synchronize_geolpda ] ]
+;}}}
+; Open sqlite geolpda, get data:{{{ } } }
+print "Open GeolPDA database..."
+change-dir dir_geolpda_local
+copy-file %geolpda %geolpda_copy.db
+	; => not terrible; this file copy is /only due to the 
+	;    fact that the btn (better than nothing) driver does
+	;    not support sqlite file without extension...
+db: open to-url rejoin [{btn://localhost/} dir_geolpda_local {geolpda_copy.db}]
+
+; Get data: as db is the same name as defined for default
+; database connexion in gll_routines.r, we can use the functions:
+; observations: {{{ } } }
+;run_query "SELECT * FROM poi"
+; MODIF: only retrieve data with _id higher than max_waypoint_name in database:
+run_query (rejoin ["SELECT * FROM poi WHERE _id > " max_waypoint_name])
+
+; Comparison of field list: to be sure that the table structure matches the 
+; one used at the time of coding (23-Oct-2013/9:24:01+2:00)
+unless sql_result_fields = ["_id" "poiname" "poitime" "elevation" "poilat" "poilon" "photourl" "audiourl" "note"] [
+	print "ATTENTION! field names differ from geolpda reference implementation"
+	print "Error, halting"
+	halt
+]
+geolpda_observations:        copy sql_result
+geolpda_observations_fields: copy sql_result_fields
+print rejoin [tab length? geolpda_observations " records in observations table from GeolPDA with _id superior to maximum waypoint_name in field_observations (" max_waypoint_name ")"]
+
+; To check if a record was previously inserted in the database, make a copy of identifiers:
+;run_query "SELECT poiname, _id, poitime FROM poi"
+;=> useless now
+tt_observations_in_geolpda: copy sql_result
+;}}}
+; orientations:{{{ } } }
+;run_query "SELECT * FROM orientation"
+; MODIF: only retrieve data with _id higher than max_waypoint_name in database:
+run_query (rejoin ["SELECT * FROM orientation WHERE _id > " max_waypoint_name])
+
+; Comparison of field list: to be sure that the table structure matches the 
+; one used at the time of coding (23-Oct-2013/9:24:01+2:00)
+unless sql_result_fields = ["_id" "poi_id" "orientationtype" "rot1" "rot2" "rot3" "rot4" "rot5" "rot6" "rot7" "rot8" "rot9" "v1" "v2" "v3"] [
+	print "ATTENTION! field names differ from geolpda reference implementation"
+	print "Error, halting"
+	halt
+]
+; If we reached here, we are ok; now, it is necessary to also fetch the full id from observations by JOINing:
+run_query rejoin ["SELECT poiname, orientation._id, poi_id, orientationtype, rot1, rot2, rot3, rot4, rot5, rot6, rot7, rot8, rot9, v1, v2, v3 FROM orientation LEFT JOIN poi ON poi._id = orientation.poi_id WHERE orientation.poi_id > " max_waypoint_name]
+geolpda_orientations: 			copy sql_result
+geolpda_orientations_fields: 	copy sql_result_fields
+print rejoin [tab length? geolpda_orientations " records in orientations measurements table from GeolPDA with _id superior to maximum waypoint_name in field_observations (" max_waypoint_name ")"]
+
+; USELESS:
+comment [
+; To check if a record was previously inserted in the database, make a copy of identifiers:
+;run_query "SELECT poi_id FROM orientation"
+; =>    ah, no: in final database, the whole id is mentioned, not the index;
+;       so we must JOIN the two tables:
+run_query "SELECT poiname, count(*) FROM orientation LEFT JOIN poi ON poi._id = orientation.poi_id GROUP BY poiname"
+tt_orientations_in_geolpda: copy sql_result
+
+; The sqlite driver apparently returns strings instead of numerics:
+; conversion to decimal! (same as value returned by postgresql driver), 
+; to allow comparisons, later on:
+tt: copy []
+foreach v tt_orientations_in_geolpda [
+	append/only tt compose [(v/1) (to-decimal v/2)]
+]
+tt_orientations_in_geolpda: copy tt
+]
+;}}}
+;}}}
+
 ; Generate a new datasource: TODO: {{{ } } }
 datasource: 9999    ; bidon; TODO generate a new datasource record in lex_datasource
 ;}}}
@@ -284,6 +300,7 @@ datasource: 9999    ; bidon; TODO generate a new datasource record in lex_dataso
 ;####################################################################
 ;/*} }}*/
 
+connection_db		; => careful: now DB points to the default database, not to the geolpda any more.
 ; Put data:{{{ } } }
 ; observations:{{{ } } }
 ; build a SQL INSERT statement:
@@ -292,7 +309,7 @@ sql_string: copy ""
 if (length? geolpda_observations) > 0 [
 	; continue the SQL INSERT statement:
 	_: {, } ; just to save a few keystrokes to the coder... and to make REJOIN code a bit more readable; variable was called SEP for SEParator; renamed to _ for clarity/brevity
-	sql_string:  {INSERT INTO public.field_observations (opid,year,obs_id,date,waypoint_name,x,y,z,description,code_litho,code_unit,srid,geologist,icon_descr,comments,sample_id,datasource,photos,audio,timestamp_epoch_ms) VALUES }
+	sql_string:  {INSERT INTO public.field_observations (device,opid,year,obs_id,date,waypoint_name,x,y,z,description,code_litho,code_unit,srid,geologist,icon_descr,comments,sample_id,datasource,photos,audio,timestamp_epoch_ms) VALUES }
 	foreach o geolpda_observations [
 		; assign temporary variables with field names: (bof, no){{{ } } }
 		;i: 0
@@ -306,14 +323,14 @@ if (length? geolpda_observations) > 0 [
 		; changed, corrected, updated, etc. since it was imported from GeolPDA.
 		; DEBUG o: geolpda_observations/4
 		tmp: to-date epoch-to-date (to-integer ((to-decimal o/3) / 1000))
-		append sql_string rejoin [newline {(} opid _ tmp/year _ {'} o/2 {'} _ {'} tmp/year "-" pad tmp/month 2 "-" pad tmp/day 2 {'} _ {'} o/1 {'} _ o/6 _ o/5 _ o/4 _ {'} o/9 {'} _ {NULL, NULL, 4326} _ {'} geologist {'} _ {NULL, NULL, NULL, NULL, } {'} o/7 {'} _ {'} o/8 {'} _ o/3 {),}]
+		append sql_string rejoin [newline {('} geolpda_device {'} _ opid _ tmp/year _ {'} o/2 {'} _ {'} tmp/year "-" pad tmp/month 2 "-" pad tmp/day 2 {'} _ {'} o/1 {'} _ o/6 _ o/5 _ o/4 _ {'} o/9 {'} _ {NULL, NULL, 4326} _ {'} geologist {'} _ {NULL, NULL, NULL, NULL, } {'} o/7 {'} _ {'} o/8 {'} _ o/3 {),}]
 	]
 	sql_string: rejoin [copy/part sql_string ((length? sql_string) - 1) ";"]
 ]
 ;DEBUG editor sql_string
 
 print "SQL statement to be run:"
-print copy/part sql_string 300
+prin copy/part sql_string 300
 print "..."
 
 
@@ -356,29 +373,12 @@ if (length? geolpda_orientations) > 0 [
 ]
 
 print "SQL statement to be run:"
-print copy/part sql_string 300
+prin copy/part sql_string 300
 print "..."
 
 insert db sql_string
 print "Done..."
 insert db "COMMIT;"
-
-; DEBUG vérif:
-run_query "SELECT * FROM public.field_observations"
-write %azeq  newline
-ww: copy sql_result
-foreach r ww [
-	write/append %azeq rejoin [mold r newline]
-]
-
-run_query "SELECT * FROM public.field_observations_struct_measures"
-write/lines %aqez sql_result_csv
-sql_result
- %azeq  newline
-ww: copy sql_result
-foreach r ww [
-	write/append %azeq rejoin [mold r newline]
-]
 
 
 ;}}}
@@ -393,6 +393,30 @@ print "commited => end."
 
 ;}}}
 
+; DEBUG vérif:  ============================ sauve 2 tables obs et mesures en csv APRÈS: {{{ } } }
+run_query "SELECT * FROM public.field_observations"
+;write %~/field_observations_apres.csv sql_result_csv
+write %~/field_observations_apres.csv newline
+ww: copy sql_result
+foreach r ww [
+	write/append %~/field_observations_apres.csv rejoin [mold r newline]
+]
+
+run_query "SELECT * FROM public.field_observations_struct_measures"
+;write/lines %~/field_observations_struct_measures_apres.csv sql_result_csv
+write %~/field_observations_struct_measures_apres.csv newline
+ww: copy sql_result
+foreach r ww [
+	write/append %~/field_observations_struct_measures_apres.csv rejoin [mold r newline]
+]
+;}}}
+
+; Comparaison des csv avant-après: {{{ } } }
+print {To see diffs, run:}
+print {diff ~/field_observations_avant.csv ~/field_observations_apres.csv}
+print {diff ~/field_observations_struct_measures_avant.csv ~/field_observations_struct_measures_apres.csv}
+;}}}
+
 ; Synchronize oruxmaps tracklogs, optionally: ;/*{{{*/ } } }
 if confirm {Synchronize oruxmaps tracklogs?} [
 	alert {locate where android device is mounted, pickup "oruxmaps" subdirectory}
@@ -400,7 +424,7 @@ if confirm {Synchronize oruxmaps tracklogs?} [
 	alert {locate the local directory where oruxmaps tracks data is (or will be) replicated}
 	unless DEBUG [ dir_oruxmaps_local:         request-dir/title/dir {locate local directory for replication of geolpda data}                        dir_oruxmaps_local         ]
 	print rejoin ["Mount directory of OruxMaps android device: " tab tab dir_mount_oruxmaps_android newline "Local directory for oruxmaps data replication: " tab dir_oruxmaps_local]
-	unless DEBUG [ if ( confirm "synchronize geolpda data?" ) [ synchronize_oruxmaps_tracklogs ] ]
+	unless DEBUG [ if ( confirm "synchronize oruxmaps tracklogs?" ) [ synchronize_oruxmaps_tracklogs ] ]
 ]
 
 ;/*}}}*/
