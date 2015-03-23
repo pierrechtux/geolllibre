f0a406ae48d6f6ab9e23275238fb3477c7cdbeed
Pierre <pierrechevaliergeol@free.fr>
2015-03-23 14:27:36 +0100

    Added a header, grepped from a version on durandeux.

--- gll_bdexplo_generation_doc.r
+++ gll_bdexplo_generation_doc.r
@@ -1,5 +1,44 @@
 #!/usr/bin/rebol -qs
-rebol []/*{{{*/ } } }
+rebol	[
+	Title:   "Generation of a HTML documentation from bdexplo database"
+	Name:    gll_bdexplo_generation_doc.r
+	Version: 0.0.1
+	Date:    "26-Sep-2013/15:08:19+2:00"
+	Author:  "Pierre Chevalier"
+	License: {
+This file is part of GeolLLibre software suite: FLOSS dedicated to Earth Sciences.
+###########################################################################
+##          ____  ___/_ ____  __   __   __   _()____   ____  _____       ##
+##         / ___\/ ___// _  |/ /  / /  / /  /  _/ _ \ / __ \/ ___/       ##
+##        / /___/ /_  / / | / /  / /  / /   / // /_/_/ /_/ / /_          ##
+##       / /_/ / /___|  \/ / /__/ /__/ /___/ // /_/ / _, _/ /___         ##
+##       \____/_____/ \___/_____/___/_____/__/_____/_/ |_/_____/         ##
+##                                                                       ##
+###########################################################################
+  Copyright (C) 2013 Pierre Chevalier <pierrechevaliergeol@free.fr>
+ 
+    GeolLLibre is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+    or write to the Free Software Foundation, Inc., 51 Franklin Street, 
+    Fifth Floor, Boston, MA 02110-1301, USA.
+    See LICENSE file.
+	}
+	Description: {
+	}
+	History: [
+	]
+]
+
 do load to-file system/options/home/bin/gll_routines.r	; Récupération des routines (et des préférences)
 connection_db						; connection à la base
 tables: run_query "SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tableowner <> 'postgres';"
@@ -73,6 +112,5 @@ append output rejoin [newline "</body></html>"]
 ;print output
 write %tt.html output
 ;browse %tt.html
-;/*}}}*/
 
 
