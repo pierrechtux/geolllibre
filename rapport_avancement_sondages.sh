825e4c2904bdc6abe2f5be635c36430ec5e5e162
Pierre <pierrechevaliergeol@free.fr>
2015-09-16 12:08:48 +0200

    Remove some fields which had been removed from dh_collars table: drill hole lengths of various sizes (was useful for invoice checking, but removed).

--- rapport_avancement_sondages.sh
+++ rapport_avancement_sondages.sh
@@ -1,7 +1,7 @@
 horodate=`date +%Y_%m_%d_%Hh%M`
 
 filename1=sondages_tetes_$horodate.csv
-sql2csv "SELECT id, date_start AS date_début, date_completed AS date_fin, x, y, z, azim_ng AS azimut_ng, dip_hz AS plongement, length AS longueur, len_destr AS long_destructif, len_pq AS long_PQ, len_hq AS long_HQ, completed::integer AS fini, comments AS commentaires FROM dh_collars WHERE opid = 18 AND (id ILIKE 'I%' OR id ILIKE 'S%' OR id ILIKE 'PJ%') ORDER BY date_start, id" > $filename1
+sql2csv "SELECT id, date_start AS date_début, date_completed AS date_fin, x, y, z, azim_ng AS azimut_ng, dip_hz AS plongement, length AS longueur, /*len_destr AS long_destructif, len_pq AS long_PQ, len_hq AS long_HQ,*/ completed::integer AS fini, comments AS commentaires FROM dh_collars WHERE opid = 18 AND (id ILIKE 'I%' OR id ILIKE 'S%' OR id ILIKE 'PJ%') ORDER BY date_start, id" > $filename1
 filename2=sondages_echantillons_teneurs_$horodate.csv
 sql2csv "SELECT id, depfrom, depto, mineralised_interval, sample_id, weight_kg AS poids_kg, core_loss_cm AS perte_k_cm, aumaxi_ppm, graph_aumaxi FROM dh_sampling_mineralised_intervals_graph_au6 WHERE opid = 18 ORDER BY id, depto" > $filename2
 filename3=sondages_passes_mineralisees_$horodate.csv
